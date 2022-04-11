8dffbf0bc1370b89fe96985c1c195aed8b17389f
Doni <dok246@gmail.com>
2022-04-11 12:31:25 +0300

    added jswebtoken

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGOURI: "mongodb+srv://Doni:ecommerce123@cluster0.t2xsz.mongodb.net/E-Commerce?retryWrites=true&w=majority",
+    MONGOURI: 'mongodb+srv://Doni:ecommerce123@cluster0.t2xsz.mongodb.net/E-Commerce?retryWrites=true&w=majority',
+    JWT_SECRET: 'asdfghjkl'
 }
