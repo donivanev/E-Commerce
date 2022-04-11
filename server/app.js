faaff8c443324ced402830f30c30e05951b2c390
Doni <dok246@gmail.com>
2022-04-11 11:05:36 +0300

    made a connection to MongoDB with authentication

--- server/app.js
+++ server/app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 4000
+const {MONGOURI} = require('./keys')
 
-app.get('/', (req, res) => {
-    res.send("Hello world!")
+mongoose.connect(MONGOURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB')
+})
+
+mongoose.connection.on('error', () => {
+    console.log('Error while connecting to MongoDB')
 })
 
 app.listen(PORT, () => {
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://Doni:ecommerce123@cluster0.t2xsz.mongodb.net/E-Commerce?retryWrites=true&w=majority",
+}
