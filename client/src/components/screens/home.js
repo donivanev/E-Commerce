cae957a4beeedca884bac98fc995cee5db57568c
Doni <dok246@gmail.com>
2022-04-12 15:10:05 +0300

    added template of a product

--- client/src/components/screens/home.js
+++ client/src/components/screens/home.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div class="row">
+            <div class="col s12 m3">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://www.pngitem.com/pimgs/m/325-3256461_our-product-range-icon-vector-shopping-cart-hd.png" />
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
