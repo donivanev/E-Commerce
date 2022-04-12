249a22c288e082fcb545d9bc09071da04002dc20
Doni <dok246@gmail.com>
2022-04-12 14:57:35 +0300

    added navbar screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
-import NavBar from './components/navbar';
+import './App.css'
+import NavBar from './components/navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/screens/home'
+import Signup from './components/screens/signup'
+import Signin from './components/screens/signin'
+import Profile from './components/screens/profile'
 
 function App() {
   return (
-    <NavBar />
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/signin" element={<Signin />}></Route>
+        <Route exact path="/signup" element={<Signup />}></Route>
+        <Route exact path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <nav>
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo left">Logo</a>
+                <Link to="/" className="brand-logo left">E-Commerce</Link>
                 <ul id="nav-mobile" className="right">
-                    <li><a href="sass.html">Sign up</a></li>
-                    <li><a href="badges.html">Sign in</a></li>
-                    <li><a href="collapsible.html">Profile</a></li>
-                    <li><a href="sass.html">Sign out</a></li>
+                    <li><Link to="/signup">Sign up</Link></li>
+                    <li><Link to="/signin">Sign in</Link></li>
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li><Link to="/signout">Sign out</Link></li>
                 </ul>
             </div>
         </nav>
