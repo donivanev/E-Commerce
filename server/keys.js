abff4a53b2056dd869b86491141f95becb9a2b15
Doni <dok246@gmail.com>
2022-05-17 13:46:21 +0300

    check for admin permissions and made product view

--- server/keys.js
+++ server/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
     MONGOURI: 'mongodb+srv://Doni:ecommerce123@cluster0.t2xsz.mongodb.net/E-Commerce?retryWrites=true&w=majority',
-    JWT_SECRET: 'asdfghjkl'
+    JWT_SECRET: 'asdfghjkl',
+    ADMIN_USERNAME: 'Admin',
+    ADMIN_PASSWORD: '$2a$12$eoNBa9RetRGe2K7Tdm/wZ.BXfsIp/wUgVk/xqvnFxqP77F49oWLoi'
 }
